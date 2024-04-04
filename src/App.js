9506bff27403d44af5aa2ba06382903800721398
NikeshSoni <nikesh21soni@gmail.com>
2024-04-04 21:10:33 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './SCSS/App.scss';
+import './SCSS/comman.scss';
+import  Shop from './pages/shop';
+import About from './pages/About';
+import Soni from './pages/Support';
+import More from './pages/More';
+
+import { Routes , Route } from "react-router-dom";  
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+            <div>
+                    <Routes>
+                        <Route index path="/" element= {<Shop />} />
+                        <Route  path="/About" element= {<About />} />
+                        <Route  path="/Support" element= {<Soni />} />
+                        <Route  path="/More" element= {<More />} />
+                    </Routes>
+            </div>
+       );
+     }
 
 export default App;
